8046cc44a9c699ad821d202dd9cb6011e1d8d42f
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-09-05 11:14:07 -0500

    Pass HttpOptions to response readers

---
+++ Source/Miruken.AspNet/HttpRouteAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Miruken.AspNet
+{
+    using System;
+    using System.Net.Http.Formatting;
+    using System.Web.Http.Controllers;
+    using Http;
+
+    public class HttpRouteAttribute
+        : Attribute, IControllerConfiguration
+    {
+        public void Initialize(
+            HttpControllerSettings controllerSettings, 
+            HttpControllerDescriptor controllerDescriptor)
+        {
+            controllerSettings.Services.Replace(typeof(IContentNegotiator), 
+                new JsonContentNegotiator(HttpFormatters.JsonTyped));
+        }
+    }
+}
--- Source/Miruken.AspNet/HttpRouteController.cs
+++ Source/Miruken.AspNet/HttpRouteController.cs
@@ -4,6 +4,7 @@
     using System.Web.Http;
     using Http;
 
+    [HttpRoute]
     public class HttpRouteController : ApiController
     {
         [HttpPost]
