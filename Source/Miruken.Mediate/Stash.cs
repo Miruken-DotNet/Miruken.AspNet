988be3ee4fd9ef2e3e1e27e92a4be115a9c6a9f0
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-12-25 09:47:14 -0600

    Updated Miruken

--- Source/Miruken.Mediate/Stash.cs
+++ Source/Miruken.Mediate/Stash.cs
@@ -66,7 +66,7 @@
             if (handler == null)
                 throw new ArgumentNullException(nameof(handler));
             _handler = handler;
-            _stash = handler.Proxy<IStash>();
+            _stash   = handler.Proxy<IStash>();
         }
 
         public T Value
