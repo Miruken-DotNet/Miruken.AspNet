3de7779303c41bdc9f34b1aca57cfabe23aa322d
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2018-07-18 08:26:39 -0500

    Added PassThroughRouter:

---
+++ Source/Miruken.Mediate/Route/PassThroughRouter.cs
@@ -0,0 +1,19 @@
+﻿namespace Miruken.Mediate.Route
+{
+    using Callback;
+    using Concurrency;
+    using Mediate;
+
+    public class PassThroughRouter : Handler
+    {
+        public const string Scheme = "pass-through";
+
+        [Handles]
+        public Promise Route(Routed request, IHandler composer)
+        {
+            return request.Route == Scheme
+                 ? composer.Send(request.Message)
+                 : null;
+        }
+    }
+}
