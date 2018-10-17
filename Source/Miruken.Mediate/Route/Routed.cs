f405b22de186669c0fd22034299f1d946472854b
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2018-10-17 08:11:55 -0500

    Made RoutedRequest cacheable

--- Source/Miruken.Mediate/Route/Routed.cs
+++ Source/Miruken.Mediate/Route/Routed.cs
@@ -14,6 +14,25 @@
 
         public string Route { get; set; }
         public string Tag   { get; set; }
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (other?.GetType() != GetType())
+                return false;
+
+            return other is Routed otherRouted
+                   && Equals(Route, otherRouted.Route)
+                   && Equals(Tag, otherRouted.Tag);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Route?.GetHashCode() ?? 0) * 31 +
+                   (Tag?.GetHashCode() ?? 0);
+        }
     }
 
     public class RoutedRequest<TResponse> : Routed, IRequest<TResponse>
@@ -31,6 +50,18 @@
         {
             get => (IRequest<TResponse>) Message;
             set => Message = value;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (!base.Equals(other)) return false;
+            return other is RoutedRequest<TResponse> otherRequest
+                   && Equals(Request, otherRequest.Request);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() * 31 + (Request?.GetHashCode() ?? 0);
         }
     }
 }
