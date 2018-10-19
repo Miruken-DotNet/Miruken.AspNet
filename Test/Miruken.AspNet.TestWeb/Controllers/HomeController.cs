14071b4bdc3a872bc5f53e3c902ded2642c7145a
Michael Dudley <michael.dudley@improving.com>
2018-10-18 19:47:13 -0500

    Adding a test for Miruken.Http to Miruken.AspNet.TestWeb

--- Test/Miruken.AspNet.TestWeb/Controllers/HomeController.cs
+++ Test/Miruken.AspNet.TestWeb/Controllers/HomeController.cs
@@ -1,14 +1,40 @@
-﻿using System.Web.Mvc;
-
+﻿
 namespace Miruken.AspNet.TestWeb.Controllers
 {
+    using System;
+    using System.Web.Mvc;
+    using Castle.Tests;
+    using Http;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
+            TestMirukenHttpFromMVC();
+
             return View();
+        }
+
+        public void TestMirukenHttpFromMVC()
+        {
+            var uri = $"{Request.Url.Scheme}://{Request.Url.Authority}/process";
+
+            var message = new CreatePlayer
+            {
+                Player = new Player
+                {
+                    Name = "a",
+                    Person = new Person
+                    {
+                        DOB = DateTime.Now
+                    }
+                }
+            };
+
+            HttpApiClient.Send(message, uri)
+                .GetAwaiter().GetResult();
         }
     }
 }
